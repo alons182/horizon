b684d95f9f8d001e67c52be18004aa8f66e04d69
alonso <alons182@hotmail.com>
2014-10-30 15:39:22 -0600

    fix about

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -4,7 +4,7 @@
  
     <div class="inner">
         <div class="content">
-        	<h1>Quienes somos?</h1>
+        	<h1>About us</h1>
 			<p>We are a Costarican company dedicated to rent and sell some of the prime properties surrounding the most beautiful beaches in the Northern Paciﬁc of Guanacaste and also in the historical Town of Liberia. We aim to provide the most personable service, we understand you want the beginning of your new life to be a seamless transition and we know we can achieve it; showing you the Pura Vida style that you are truly looking for. We are constantly innovating to be the most complete Real Estate Ofﬁce in the area, one where you are not only buying property but you are ﬁnding a solution to your living wants and needs. </p>
 			<h2>Vision</h2>
 			<p>Be the most important ally for our customers while buying or renting a property; demonstrating empathy and solid support during the length of the process. Become the leaders in the industry, the most solid, stable, real estate ofﬁce in the province of Guanacaste. ! </p>
